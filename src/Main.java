b92e2e61943180265fdee1bde1a74bae9fe340d5
HugTheMainframe <rasm65n4@stud.kea.dk>
2024-02-21 22:00:22 +0100

    User Storie 1: As a user i would like to all movies has a title and a director. User Storie 2: Only some movies are in color. User Storie 3: i want to register year and genre of the movie.
    Test: See if creating an instance, that it is possible to output instance fields/attributes. (works)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        //inout object plus user input fields per movie
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the title of the film: ");
+        String title = input.nextLine();
+        System.out.print("Enter the director of the film: ");
+        String director = input.nextLine();
+        System.out.print("Enter the year of the film: ");
+        int yearCreated = input.nextInt();
+        System.out.print("Enter if the film is in color(true or false): ");
+        boolean isInColor = input.nextBoolean();
+        System.out.print("Enter the length of the film(in minuts): ");
+        int lengthInMinuts = input.nextInt();
+        System.out.print("Enter the genre of the film: ");
+        //must be next else it skips the input
+        String genre = input.next();
+
+        //testing object for input (del 1)
+        Movie movieOne = new Movie(title, director, yearCreated,
+                isInColor, lengthInMinuts, genre);
+
+
+        //testing for output of the objects getters (del 1)
+        System.out.println(movieOne.getTitle() + "\n" + movieOne.getDirector() + "\n" +
+                movieOne.getYearCreated() + "\n" + movieOne.getIsinColor() + "\n" +
+                 movieOne.getLengthInMinuts() + "\n" + movieOne.getGenre());
+    }
 
 
 }
